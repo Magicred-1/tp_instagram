16441773f264af27f12a353325fec4344c47e58a
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2021-12-16 15:25:47 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function signin()
+function sign_in()
 {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     if ( username == "djason.gadiou.sio" && password == "formget#123"){
     alert ("Login successfully");
-    window.location = "success.html";
+    window.location = "accueil.html";
     return false;
 }
 }
