658d080b5a67e508d39e1c1d9f441d9b93cc3b81
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-01-05 15:35:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Loading ...");
     let btn, warning, email, password;
-    let exprmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-    let exprmdp = /^[a-zA-Z0-9._-]{2,15}$/;
+    let regexmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    let regexmdp = /^[a-zA-Z0-9._-]{2,15}$/;
 
     warning = document.querySelector(".form p");
     email = document.getElementsByTagName("input")[0].value;
@@ -18,18 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
         email = document.getElementsByTagName("input")[0].value;
         password = document.getElementsByTagName("input")[1].value;
 
-        if (email.match(exprmail) && password.match(exprmdp)) {
+        if (email.match(regexmail) && password.match(regexmdp)) {
             warning.classList.add("success");
             warning.classList.remove("error");
             warning.innerText = "Connexion réussie";
             localStorage.setItem("user", email);
             sessionStorage.setItem("user", "sessionid");
             document.location.assign("accueil.html");
-        } else if (password.match(exprmdp)) {
+        } else if (password.match(regexmdp)) {
             warning.classList.add("error");
             warning.innerText = "Mail invalide";
             warning.classList.remove("success");
-        } else if (email.match(exprmail)) {
+        } else if (email.match(regexmail)) {
             warning.classList.add("error");
             warning.innerText = "Mot de passe invalide";
             warning.classList.remove("success");
