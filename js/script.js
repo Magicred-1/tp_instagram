c82bfa10ab89c344fb6dacceb5de4fba3832d3f2
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-01-06 15:28:28 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Loading ...");
-    let btn, warning, email, password;
     let regexmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
     let regexmdp = /^[a-zA-Z0-9._-]{2,15}$/;
 
-    warning = document.querySelector(".form p");
-    email = document.getElementsByTagName("input")[0].value;
-    password = document.getElementsByTagName("input")[1].value;
-    btn = document.getElementsByTagName("input")[2];
+    let warning = document.querySelector(".form p");
+    let email = document.getElementsByTagName("input")[0].value;
+    let password = document.getElementsByTagName("input")[1].value;
+    let btn = document.getElementsByTagName("input")[2];
 
     btn.addEventListener("click", e => {
         e.preventDefault();
